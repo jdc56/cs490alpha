1d162cd6a886aa692d6396766f7f508dc3cdcbde
Cindy <cindyhtai@gmail.com>
2021-09-20 23:37:02 -0400

    Initial commit of frontend php files

---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+</head>
+
+<body>
+    <form action="" method=""> <!-- insert authenticate.php and method --> 
+        <div class="login">
+            <h1>Login</h1>
+            <label for="user">Username:</label>
+            <input type="text" id="user" name="user"><br><br>
+
+            <label for="pw">Password:</label>
+            <input type="text" id="pw" name="pw"><br><br>
+            
+            <input type="submit" value="Sign In">
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ user.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>User Page</title>
+</head>
+
+<body>
+    <form action="" method=""> <!-- insert authenticate.php and method --> 
+        <div class="login">
+            <h1>Hello User!</h1>
+            <p>This page is for the regular user. The admin should be redirected to the admin page.</p>
+        </div>
+    </form>
+</body>
+
+</html>
