1519f13b9a76a6f4375d56fb3e111dde2f2aeeb3
Cindy <cindyhtai@gmail.com>
2021-09-21 10:40:43 -0400

    Test database query

--- authenticate.php
+++ authenticate.php
@@ -10,6 +10,7 @@ try {$db = parse_url(getenv("DATABASE_URL"));
         $db["pass"],
         ltrim($db["path"], "/")
     ));
+    echo "Database connection successful. <br/>";
 }catch (PDOException $e) {
     echo "Error : " . $e->getMessage() . "<br/>";
     die();
@@ -19,7 +20,7 @@ try {$db = parse_url(getenv("DATABASE_URL"));
 $username = "";
 $pw = "";
 
-/*
+
 // SQL Query to test database user fetch
 $sql = 'SELECT Username, "Password", "Admin" FROM users ORDER BY Username;';
 $res = pg_query($conn, $sql);
@@ -27,6 +28,14 @@ if (!$res) {
   echo "An error occurred.\n";
   exit;
 }
+else {
+    echo "Query executed<br/>";
+    while ($row = pg_fetch_row($result)) {
+        echo "User: $row[1]  Pass: $row[2] Is Admin: $row[3]";
+        echo "<br/>";
+      }
+}
+/*
 while ($row = pg_fetch_row($result)) {
     echo "User: $row[1]  Pass: $row[2] Is Admin: $row[3]";
     echo "<br />\n";
